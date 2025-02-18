291921808e43479bc73f62bc3c19a4f72cebf1fd
Batuhan Ergun <ergunbatuhan@icloud.com>
2025-02-18 16:33:25 +0100

    lora transmit

--- sensor/src/main.cpp
+++ sensor/src/main.cpp
@@ -3,15 +3,25 @@
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BNO055.h>
 #include <utility/imumaths.h>
-  
+
+// Define UART for AS32 module - using default UART0
+#define LORA_SERIAL Serial1
+#define LORA_TX_PIN 0  // GP0 - UART0 TX
+#define LORA_RX_PIN 1  // GP1 - UART0 RX
+
 Adafruit_BNO055 bno = Adafruit_BNO055(55);
+
+// LoRa parameters
+const byte nodeID = 0x02;      // This device's ID
+const byte baseStation = 0xFF;  // Destination ID (base station)
 
 void setup(void) 
 {
   Serial.begin(9600);
-  Serial.println("Orientation Sensor Test"); Serial.println("");
+  LORA_SERIAL.begin(9600);
   
-  /* Initialise the sensor */
+  Serial.println("Orientation Sensor Test");
+  
   if(!bno.begin())
   {
     Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
@@ -19,24 +29,32 @@ void setup(void)
   }
   
   delay(1000);
-    
   bno.setExtCrystalUse(true);
+}
+
+void sendLoRaPacket(String data) {
+  // Format: [sender][recipient][length][data]
+  LORA_SERIAL.write(nodeID);          // Sender ID
+  LORA_SERIAL.write(baseStation);     // Recipient ID
+  LORA_SERIAL.write(data.length());   // Package length
+  LORA_SERIAL.print(data);            // Data
 }
 
 void loop(void) 
 {
-  /* Get a new sensor event */ 
   sensors_event_t event; 
   bno.getEvent(&event);
   
-  /* Display the floating point data */
-  Serial.print("X: ");
-  Serial.print(event.orientation.x, 4);
-  Serial.print("\tY: ");
-  Serial.print(event.orientation.y, 4);
-  Serial.print("\tZ: ");
-  Serial.print(event.orientation.z, 4);
-  Serial.println("");
+  // Format data as JSON string
+  String data = "{\"x\":" + String(event.orientation.x, 4) + 
+                ",\"y\":" + String(event.orientation.y, 4) + 
+                ",\"z\":" + String(event.orientation.z, 4) + "}";
+  
+  // Send data over LoRa
+  sendLoRaPacket(data);
+  
+  // Debug print
+  Serial.println("Sent: " + data);
   
   delay(100);
 }
